517c1a3cabbc66e3f424fd400e2b39e074a6483a
yang <yrzcode@hotmail.com>
2023-09-15 23:55:55 +0900

    Build platform selector

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -18,8 +18,8 @@ const GameGrid = ({ selectedGenre }: Props) => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding="10px"
         spacing={3}
+        marginTop="10px"
       >
         {(isLoading && (
           <>
