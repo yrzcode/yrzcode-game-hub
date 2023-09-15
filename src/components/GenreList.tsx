c5ff9ac8092104a6f088412caf0ce0fc3ff11edb
yang <yrzcode@hotmail.com>
2023-09-15 22:28:36 +0900

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,27 @@
+import { Text, HStack, Image, List, ListItem } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
+import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
   const { data: genres } = useGenres();
 
   return (
-    <ul>
+    <List>
       {genres.map((genre) => {
-        return <li key={genre.id}>{genre.name}</li>;
+        return (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        );
       })}
-    </ul>
+    </List>
   );
 };
 
