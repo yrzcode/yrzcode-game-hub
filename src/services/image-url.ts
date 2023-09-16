26403806933ad07aae739565f4279c103038876e
yang <yrzcode@hotmail.com>
2023-09-16 15:48:21 +0900

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/no-image.jpeg';
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return '';
+  if (!url) return noImage;
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
   const res = url.slice(0, index) + 'crop/600/400/' + url.slice(index);
