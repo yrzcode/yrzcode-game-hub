01ed061c790a50d532a7b5dc068f4a8029a778ab
rz_yang <yrzcode@hotmail.com>
2025-04-30 01:53:11 +0900

    adjust card styles

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,22 +1,22 @@
-import bullsEye from '../assets/bulls-eyes.png';
-import thumbsUp from '../assets/thumb-up.png';
-import meh from '../assets/meh.png';
-import { Image, ImageProps } from '@chakra-ui/react';
+import bullsEye from "../assets/bulls-eyes.png";
+import thumbsUp from "../assets/thumb-up.png";
+import meh from "../assets/meh.png";
+import { Image, type ImageProps } from "@chakra-ui/react";
 
 interface Props {
-  rating: number;
+	rating: number;
 }
 
 const Emoji = ({ rating }: Props) => {
-  if (rating < 3) return null;
+	if (rating < 3) return null;
 
-  const emojiMap: { [key: number]: ImageProps } = {
-    3: { src: meh, alt: 'meh', boxSize: '25px' },
-    4: { src: thumbsUp, alt: 'recommended', boxSize: '25px' },
-    5: { src: bullsEye, alt: 'exceptional', boxSize: '35px' },
-  };
+	const emojiMap: { [key: number]: ImageProps } = {
+		3: { src: meh, alt: "meh", boxSize: "25px" },
+		4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
+		5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
+	};
 
-  return <Image {...emojiMap[rating]} mt={1} />;
+	return <Image {...emojiMap[rating]} />;
 };
 
 export default Emoji;
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,34 +1,41 @@
-import { Card, CardBody, Heading, Image, HStack } from '@chakra-ui/react';
-import { Game } from '../hooks/useGames';
-import PlatformIconList from './PlatformIconList';
-import CriticScore from './CriticScore';
-import getCroppedImageUrl from '../services/image-url';
-import Emoji from './Emoji';
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
+import type { Game } from "../hooks/useGames";
+import { Card, CardBody, Heading, Image, HStack } from "@chakra-ui/react";
 
 interface Props {
-  game: Game;
+	game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
-  return (
-    <Card>
-      <Image
-        objectFit="cover"
-        src={getCroppedImageUrl(game.background_image)}
-      />
-      <CardBody>
-        <HStack justifyContent="space-between" mb={3}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Heading fontSize="2xl">
-          {game.name} <Emoji rating={game.rating_top} />
-        </Heading>
-      </CardBody>
-    </Card>
-  );
+	return (
+		<Card>
+			<Image
+				objectFit="cover"
+				src={getCroppedImageUrl(game.background_image)}
+			/>
+			<CardBody>
+				<HStack justifyContent="space-between" mb={3}>
+					<PlatformIconList
+						platforms={game.parent_platforms.map((p) => p.platform)}
+					/>
+					<CriticScore score={game.metacritic} />
+				</HStack>
+				<Heading fontSize="2xl">
+					<HStack
+						spacing={2}
+						alignItems="center"
+						justifyContent="space-between"
+					>
+						<span>{game.name}</span>
+						<Emoji rating={game.rating_top} />
+					</HStack>
+				</Heading>
+			</CardBody>
+		</Card>
+	);
 };
 
 export default GameCard;
